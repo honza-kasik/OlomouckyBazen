af888922ace4ede5ee40e30ef30109e64d646eea
Honza Kašík <honza.kasik1@seznam.cz>
2016-11-23 15:31:21 +0100

    wip SwimmingPoolView

---
+++ app/src/main/java/cz/honzakasik/bazenolomouc/SwimmingPoolView.java
@@ -0,0 +1,14 @@
+package cz.honzakasik.bazenolomouc;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SwimmingPoolView extends View {
+
+    public SwimmingPoolView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
